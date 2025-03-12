9bc2f633cd30c847b0c34c2283ec12d4f75ee2ab
Darren3501 <darren.mahin05@gmail.com>
2025-03-12 17:59:52 -0400

    setup

---
+++ src/mahin_project2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class mahin_project2 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		int answer = in.nextInt();
+		
+		
+		
+
+	}
+	
+	public static 
+
+}
