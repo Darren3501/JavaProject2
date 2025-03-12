a5d99511216167570126351d03278d74170e3862
Darren3501 <darren.mahin05@gmail.com>
2025-03-12 18:04:34 -0400

    setup

--- src/Catch.java
+++ src/Catch.java
@@ -1,4 +1,83 @@
 
-public class Catch {
+public class Catch 
+{
+	private String license;
+	private String name;
+	private float weight;
+	private int length;
+	private int catchDay;
+	private int catchMonth;
+	private int catchYear;
+	
+	public Catch()
+	{
+		
+	}
+	
+	public Catch(String license, String name, float weight, int length, int catchDay, int catchMonth, int catchYear)
+	{
+		
+	}
 
+	public String getLicense() {
+		return license;
+	}
+
+	public void setLicense(String license) {
+		this.license = license;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getCatchDay() {
+		return catchDay;
+	}
+
+	public void setCatchDay(int catchDay) {
+		this.catchDay = catchDay;
+	}
+
+	public int getCatchMonth() {
+		return catchMonth;
+	}
+
+	public void setCatchMonth(int catchMonth) {
+		this.catchMonth = catchMonth;
+	}
+
+	public int getCatchYear() {
+		return catchYear;
+	}
+
+	public void setCatchYear(int catchYear) {
+		this.catchYear = catchYear;
+	}
+	
+	public String toString()
+	{
+		String returnMe = "";
+		return returnMe;
+	}
 }
